f310a9d39082df9d21a53c8b1ec19aa249e31583
Kristian Fulkerson <kristianf451@gmail.com>
2022-03-14 13:48:13 -0700

    trying to host

--- shareme_frontend/src/App.js
+++ shareme_frontend/src/App.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Routes, Route, useNavigate } from "react-router-dom";
 
 import { Login } from "./components";
 import Home from "./containers/Home";
+import { fetchUser } from "./utils/fetchUser";
 
 const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const user = fetchUser();
+
+    if (!user) navigate("/login");
+  }, [navigate]);
+
   return (
     <Routes>
       <Route path="login" element={<Login />} />
